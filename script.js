102509a5abd30657660f920867d1e3e6296f0864
tbq <truongbaquan@gmail.com>
2019-07-31 00:06:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-<script>
 function alertBuy() {
   var product = document.getElementById("productInput").value;
   var productNum = document.getElementById("numSelect").value;
   
   alert("You've just bought " + productNum + " " + product + "(s)!");
 }
-</script>
