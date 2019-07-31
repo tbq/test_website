b192fff1e5232829e2f9d1535c2996eb16cf12e9
tbq <truongbaquan@gmail.com>
2019-07-31 00:02:27 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+function alertBuy() {
+  var product = document.getElementById("productInput").value;
+  var productNum = document.getElementById("numSelect").value;
+  
+  alert("You've just bought " + productNum + " " + product + "(s)!");
+}
+</script>
